a87fca4e832c60e0013a9cc4dd8e067d21785ee7
Hoyt Koepke <hoytak@apple.com>
2019-06-06 10:22:45 -0700

    Cleaning up more headers.

--- src/core/logging/logging.hpp
+++ src/core/logging/logging.hpp
@@ -2,14 +2,7 @@
 #define TURI_CORE_LOGGING_HPP_
 
 #include <core/logging/logger.hpp>
-#include <core/logging/backtrace.hpp>
-#include <core/logging/log_level_setter.hpp>
-#include <core/logging/fail_method.hpp>
-#include <core/logging/error.hpp>
 #include <core/logging/assertions.hpp>
-#include <core/logging/log_rotate.hpp>
-#include <core/logging/logger_includes.hpp>
 #include <core/logging/table_printer/table_printer.hpp>
-#include <core/logging/table_printer/table_element_printers.hpp>
 
 #endif
