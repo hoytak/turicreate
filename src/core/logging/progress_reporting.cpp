91c1fd816bf0380dfadf6d0e3ce9205d4388ecf8
Hoyt Koepke <hoytak@apple.com>
2019-07-19 10:14:30 -0700

    Update.

--- src/core/logging/progress_reporting.cpp
+++ src/core/logging/progress_reporting.cpp
@@ -1,4 +1,112 @@
+#include <core/logging/logger.hpp>
+#include <core/logging/progress_reporting.cpp>
+#include <mutex>
+#include <parallel/atomic.hpp>
 
+namespace turi { 
+
+
+  static std::vector<std::weak_ptr<progress_reporter> > _current_progress_stack;
+  atomic<size_t> hook_id = 0; 
+
+  static std::vector<std::pair<size_t, std::function<void(const std::string&, size_t, size_t)> > > progress_report_hooks; 
+  static std::vector<std::pair<size_t, std::function<void(const std::string&, const std::string&, size_t)> > > task_enter_hooks;
+  static std::vector<std::pair<size_t, std::function<void(const std::string&)> > > task_completion_hooks; 
+
+
+
+  /** Set a function to be called on progress updating. 
+   *
+   */
+  size_t set_progress_hook(std::function<void(const std::string&, size_t, size_t)>&& hook);
+
+
+  size_t set_task_enter_hook(std::function<void(const std::string&, size_t)>&& hook);
+
+
+  size_t set_task_completion_hook(std::function<void(const std::string&)>&& hook); 
+
+
+
+
+
+  std::shared_ptr<progress_reporter> enter_task(const std::string& task_name, const std::string& task_description, size_t num_work_units) {
+
+
+
+
+
+
+
+
+  }
+
+
+  static void task_completed(const progress_reporter* reported) {
+     ASSERT_TRUE(_current_progress_stack.back().get() == reported); 
+
+
+     for(const auto& p : task_completion_hooks) { 
+
+
+
+     }
+
+
+
+  }
+  
+
+
+
+
+  static void 
+
+
+
+
+  class progress_reporter { 
+    public:
+
+    progress_reporter(const std::string& task_name, const std::string& task_description, size_t num_work_units); 
+
+    ~progress_reporter();
+
+    size_t increment_completed_units();
+    size_t report_work_completion(size_t work_unit_count, size_t current_num_work_units); 
+
+
+    /** Marks the current task as being completed.  
+     *
+     *  Also called on descruction. 
+     *
+     */
+    void complete();
+
+    std::shared_ptr<progress_reporter> enter_subtask(const std::string& task_name, const std::string& task_description, size_t num_work_units);
+
+
+  private:
+    std::string m_name;
+    std::string m_description;
+
+    std::once_flag m_completion_reported; 
+  }; 
+
+
+  /** Set a function to be called on progress updating.  
+   *
+   */
+  size_t set_progress_hook(std::function<void(const std::string&, size_t, size_t)>&& hook);
+
+
+  size_t set_task_enter_hook(std::function<void(const std::string&, size_t)>&& hook); 
+  size_t set_task_completion_hook(std::function<void(const std::string&, size_t)>&& hook); 
+
+  std::shared_ptr<progress_reporter> enter_task(const std::string& task_name, const std::string& task_description, size_t num_work_units);
+}
+
+#endif
 
 
 
--- src/core/logging/progress_reporting.hpp
+++ src/core/logging/progress_reporting.hpp
@@ -2,24 +2,30 @@
 #define TURI_PROGRESS_REPORTING_HPP_
 
 
-#include <
+#include <core/logging/logger.hpp>
 #include <mutex>
 
 namespace turi { 
 
+  class progress_context; 
 
+  /**   A simple progress reporting mechanism.
+   *    
+   *
+   *
+   */
   class progress_reporter { 
     public:
 
+    progress_reporter(const std::string& task_name, const std::string& task_description, size_t num_work_units); 
 
-    progress_reporter(const std::string& task_name, size_t num_work_units); 
-    progress_reporter(std::shared_ptr<progress_reporter> parent_task, const std::string& subtask_name, size_t num_work_units); 
     ~progress_reporter();
-
-
 
     size_t increment_completed_units();
     size_t report_work_completion(size_t work_unit_count, size_t current_num_work_units); 
+
+    const std::string& name() const { return m_name; } 
+    const std::string& description() const { return m_description; } 
 
 
     /** Marks the current task as being completed.  
@@ -29,31 +35,58 @@ namespace turi {
      */
     void complete();
 
-  
-    std::shared_ptr<progress_reporter> enter_subtask(const std::string& task_name, const std::string& task_description, size_t num_work_units);
-
-
-
-
-
   private:
     std::string m_name;
     std::string m_description;
-    std::shared_ptr<progress_reporter> parent;
+    std::shared_ptr<progress_context> m_owning_context; 
 
     std::once_flag m_completion_reported; 
   }; 
 
 
-  /** Set a function to be called on progress updating.  
+  class progress_context { 
+    public: 
+
+  /** Set a function to be called whenever a progress reports  
    *
    */
   size_t set_progress_hook(std::function<void(const std::string&, size_t, size_t)>&& hook);
 
+
+  size_t set_task_enter_hook(std::function<void(const std::string&, const std::string&, size_t)>&& hook);
+
+
+  size_t set_task_completion_hook(std::function<void(const std::string&)>&& hook); 
+
   std::shared_ptr<progress_reporter> enter_task(const std::string& task_name, const std::string& task_description, size_t num_work_units);
 
 
-  
+    private: 
+
+  friend class progress_reporter; 
+
+  void _task_completed(const progress_reporter* reportee);
+
+
+
+  static std::vector<std::weak_ptr<progress_reporter> > _current_progress_stack;
+
+  static std::vector<std::pair<size_t, std::function<void(const std::string&, size_t, size_t)> > > progress_report_hooks; 
+  static std::vector<std::pair<size_t, std::function<void(const std::string&, const std::string&, size_t)> > > task_enter_hooks;
+  static std::vector<std::pair<size_t, std::function<void(const std::string&)> > > task_completion_hooks; 
+
+
+
+
+
+  }
+
+
+
+
+
+  // Returns the global progress manager.
+  static const std::shared_ptr<progress_context>& global_progress_context();
 
 
 
