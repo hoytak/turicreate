a02799a65d2a7f7f5d9f26f779f144e1804b5ec7
Hoyt Koepke <hoytak@apple.com>
2019-12-17 23:23:23 -0700

    WIP.

--- src/model_server/server/registration.hpp
+++ src/model_server/server/registration.hpp
@@ -11,58 +11,86 @@
 #include <atomic>
 #include <mutex>
 #include <array>
+#include <model_server/model_server.hpp> 
 
 namespace turi {
 
+  /** Implementations of macros functions for registering a model class
+ *
+ *
+ *
+ */ 
+extern const std::type_info* __previous_model_type_info;
 
-  void process_registered_callbacks_internal();
+// A helper class to use a static initializer to do a lightweight registration 
+// of class loading at library load time.  Intended to be used as a component of 
+// the 
+class __model_server_static_class_registration_hook {
+  public:
+    inline __model_server_static_class_registration_hook(
+       const std::type_info* model_type_info, model_server_impl::registration_callback f) {
 
-  typedef void (*class_registration_callback)(void); 
-
-  extern std::array<class_registration_callback, TC_MAX_REGISTERED_CALLBACKS> registration_callback_list; 
-  extern std::atomic<size_t> _callback_pushback_index;
-  extern std::atomic<size_t> _callback_last_processed_index;
-
-  // Register the callbacks if need be.
-  static inline void process_registered_callbacks() { 
-    if(_callback_last_processed_index < _callback_pushback_index) { 
-      process_registered_callbacks_internal(); 
+      // Quick check to cut out duplicate registrations.  This can 
+      // happen, e.g. if the class or the function macros appear in a header,  
+      // which is fine and something we are designed to handle.  
+      // However, this means that multiple registration calls can occur for the same 
+      // class, and this quickly filters those registrations out. 
+      if(__previous_model_type_info != nullptr
+          && (*__previous_model_type_info) != (*model_type_info)) {
+        __previous_model_type_info = model_type_info;
+        model_server().add_registration_callback(f);
+      }
     }
-  }
+};
 
 
-  static inline void _add_registration_callback(class_registration_callback callback) {
 
-    size_t insert_index_raw = (_callback_pushback_index++);
-    
-    do {
-      // Check to make sure this can be safely inserted.
-      size_t processed_index_raw = _callback_last_processed_index;
+#define REGISTER_MODEL(model) \
+  static void register_##name(model_server_impl& server) { \
+    server.template register_new_model<model>(); \
+  } \
+  \
+  static __model_server_static_class_registration_hook __register_##model##_hook(&typeid(model), register_##model)
 
-      if(processed_index_raw + TC_MAX_REGISTERED_CALLBACKS > insert_index_raw) { 
-        break; 
-      } else {
-        // This will process the next block of insertions.
-        process_registered_callbacks();
+
+
+extern const void* __previous_registerd_function_ptr;
+
+// A helper class to use a static initializer to do a lightweight registration 
+// of class loading at library load time.  Intended to be used as a component of 
+// the 
+class __model_server_static_function_registration_hook {
+  public:
+    inline __model_server_static_function_registration_hook(
+       const void* function_ptr, model_server_impl::registration_callback f) {
+
+      // Quick check to cut out duplicate registrations.  This can 
+      // happen, e.g. if the class or the function macros appear in a header,  
+      // which is fine and something we are designed to handle.  
+      // However, this means that multiple registration calls can occur for the same 
+      // class, and this quickly filters those registrations out. 
+      if(__previous_registerd_function_ptr != nullptr
+          && __previous_registerd_function_ptr != function_ptr) {
+
+        __previous_registerd_function_ptr = function_ptr;
+
+        model_server().add_registration_callback(f);
       }
-
-    } while(true);
-
-    size_t insert_index = insert_index_raw % TC_MAX_REGISTERED_CALLBACKS;
-
-    ASSERT_TRUE(registration_callback_list[insert_index] == nullptr);
-    registration_callback_list[insert_index] = callback;
-  } 
+    }
+};
 
 
-  // A helper class to use a static initializer to do a lightweight registration 
-  // of class loading at library load time.
-  class _static_registration_hook {
-    public: 
-      inline _static_registration_hook(class_registration_callback* f) { 
-        f(); 
-      }
-  };
+
+#define REGISTER_NAMED_FUNCTION(name, function, ...) \
+\
+  static void register_function_##function(model_server_impl& server) {\
+    server.register_new_function(name, function, {__VA_ARGS__});\
+  } \
+   __model_server_static_function_registration_hook \
+__register_function_##function##_hook(register_function_##function)
+
+#define REGISTER_FUNCTION(function, ...) \
+  REGISTER_NAMED_FUNCTION(#function, function, __VA_ARGS__)
 
 
 
