e9175cf315bba3cbe7e4fd52c6baefa82e88725f
Hoyt Koepke <hoytak@apple.com>
2019-12-18 21:24:52 -0700

    Reverted to model core stuff.

--- src/model_server/server/registration.hpp
+++ src/model_server/server/registration.hpp
@@ -6,93 +6,13 @@
 #ifndef TURI_UNITY_REGSTRATION_HPP_
 #define TURI_UNITY_REGSTRATION_HPP_
 
-#define TC_MAX_REGISTERED_CALLBACKS 512
-
-#include <atomic>
-#include <mutex>
-#include <array>
-#include <model_server/model_server.hpp> 
+#include <model_server/lib/toolkit_class_registry.hpp>
+#include <model_server/lib/toolkit_function_registry.hpp>
 
 namespace turi {
 
-  /** Implementations of macros functions for registering a model class
- *
- *
- *
- */ 
-extern const std::type_info* __previous_model_type_info;
-
-// A helper class to use a static initializer to do a lightweight registration 
-// of class loading at library load time.  Intended to be used as a component of 
-// the 
-class __model_server_static_class_registration_hook {
-  public:
-    inline __model_server_static_class_registration_hook(
-       const std::type_info* model_type_info, model_server_impl::registration_callback f) {
-
-      // Quick check to cut out duplicate registrations.  This can 
-      // happen, e.g. if the class or the function macros appear in a header,  
-      // which is fine and something we are designed to handle.  
-      // However, this means that multiple registration calls can occur for the same 
-      // class, and this quickly filters those registrations out. 
-      if(__previous_model_type_info != nullptr
-          && (*__previous_model_type_info) != (*model_type_info)) {
-        __previous_model_type_info = model_type_info;
-        model_server().add_registration_callback(f);
-      }
-    }
-};
-
-
-
-#define REGISTER_MODEL(model) \
-  static void register_##name(model_server_impl& server) { \
-    server.template register_new_model<model>(); \
-  } \
-  \
-  static __model_server_static_class_registration_hook __register_##model##_hook(&typeid(model), register_##model)
-
-
-
-extern const void* __previous_registerd_function_ptr;
-
-// A helper class to use a static initializer to do a lightweight registration 
-// of class loading at library load time.  Intended to be used as a component of 
-// the 
-class __model_server_static_function_registration_hook {
-  public:
-    inline __model_server_static_function_registration_hook(
-       const void* function_ptr, model_server_impl::registration_callback f) {
-
-      // Quick check to cut out duplicate registrations.  This can 
-      // happen, e.g. if the class or the function macros appear in a header,  
-      // which is fine and something we are designed to handle.  
-      // However, this means that multiple registration calls can occur for the same 
-      // class, and this quickly filters those registrations out. 
-      if(__previous_registerd_function_ptr != nullptr
-          && __previous_registerd_function_ptr != function_ptr) {
-
-        __previous_registerd_function_ptr = function_ptr;
-
-        model_server().add_registration_callback(f);
-      }
-    }
-};
-
-
-
-#define REGISTER_NAMED_FUNCTION(name, function, ...) \
-\
-  static void register_function_##function(model_server_impl& server) {\
-    server.register_new_function(name, function, {__VA_ARGS__});\
-  } \
-   __model_server_static_function_registration_hook \
-__register_function_##function##_hook(register_function_##function)
-
-#define REGISTER_FUNCTION(function, ...) \
-  REGISTER_NAMED_FUNCTION(#function, function, __VA_ARGS__)
-
-
+  void register_functions(toolkit_function_registry& registry);
+  void register_models(toolkit_class_registry& registry);
 
 }
 
